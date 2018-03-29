ff73ec130ce2c2bb364637c050a0554c325ac274
Victor Shyba <victor1984@riseup.net>
2018-03-28 21:14:17 -0300

    separate claim name advance from advance txs

--- lbryumx/block_processor.py
+++ lbryumx/block_processor.py
@@ -4,11 +4,9 @@ from binascii import hexlify
 
 from electrumx.server.block_processor import BlockProcessor
 from lbryschema.decode import smart_decode
-from lbryschema.error import DecodeError
 from lbryschema.uri import parse_lbry_uri
-from lbryschema.error import URIParseError
 
-from lbryumx.opcodes import NameClaim, ClaimUpdate, ClaimSupport
+from lbryumx.model import NameClaim, ClaimUpdate, ClaimSupport
 
 
 class LBRYBlockProcessor(BlockProcessor):
@@ -24,35 +22,33 @@ class LBRYBlockProcessor(BlockProcessor):
 
     def advance_txs(self, txs):
         undo_info = super().advance_txs(txs)
-        get_address = self.coin.address_from_script
         height = self.height + 1
-        put_claim = self.claim_cache.__setitem__
-        claim_name_sequence = self.claim_name_sequence_cache
         for tx, txid in txs:
             if tx.has_claims:
                 for index, output in enumerate(tx.outputs):
                     claim = output.claim
                     if isinstance(claim, NameClaim):
                         claim_id = claim_id_hash(txid, index)
-                        name = claim.name
-                        value = claim.value
-                        address = get_address(output.pk_script)
-                        cert_id = None
-                        try:
-                            parse_lbry_uri(claim.name)  # skip invalid names
-                            decoded_claim = smart_decode(claim.value)
-                            cert_id = decoded_claim.certificate_id
-                        except Exception:
-                            pass
-                        put_claim(claim_id, (name, value, address, height, cert_id))
-                        claims_for_name = claim_name_sequence.get(claim.name, {})
-                        if not claims_for_name:
-                            claims_for_name[claim_id] = 1
-                        else:
-                            claims_for_name[claim_id] = max(i for i in claims_for_name.values()) + 1
-                        claim_name_sequence[claim.name] = claims_for_name
-                        # TODO: add cert_id->[signatures,]
+                        self.advance_claim_name_transaction(output, height, claim_id)
         return undo_info
+
+    def advance_claim_name_transaction(self, output, height, claim_id):
+        address = self.coin.address_from_script(output.pk_script)
+        name, value, cert_id = output.claim.name, output.claim.value, None
+        try:
+            parse_lbry_uri(name)  # skip invalid names
+            cert_id = smart_decode(value).certificate_id
+        except Exception:
+            pass
+        self.claim_cache[claim_id] = (name, value, address, height, cert_id,)
+
+        claims_for_name = self.claim_name_sequence_cache.get(name, {})
+        if not claims_for_name:
+            claims_for_name[claim_id] = 1
+        else:
+            claims_for_name[claim_id] = max(i for i in claims_for_name.values()) + 1
+        self.claim_name_sequence_cache[name] = claims_for_name
+        # TODO: add cert_id->[signatures,]
 
 
 def claim_id_hash(txid, n):
