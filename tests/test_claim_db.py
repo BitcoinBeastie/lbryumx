292149d22e0e95ce2378c47f58eb7316305349b3
Victor Shyba <victor1984@riseup.net>
2018-03-30 21:22:24 -0300

    test claim ids by cert id

--- tests/test_claim_db.py
+++ tests/test_claim_db.py
@@ -8,3 +8,10 @@ def test_claim_sequence_remove_reorders(block_processor):
     db.remove_claim_for_name(name, 'id2')
 
     assert db.get_claims_for_name(name) == {'id1': 1, 'id3': 2}
+
+
+def test_cert_to_claims_storage(block_processor):
+    db = block_processor
+    db.put_claim_id_signed_by_cert_id(b'certificate_id', b'claim_id1')
+    db.put_claim_id_signed_by_cert_id(b'certificate_id', b'claim_id2')
+    assert db.get_signed_claim_id_by_cert_id(b'certificate_id') == (b'claim_id1', b'claim_id2')
