8d935fc2b057c1b87749e81aafb3230f4191c084
Victor Shyba <victor1984@riseup.net>
2018-04-24 15:53:04 -0300

    adds getclaimsforname on daemon

--- lbryumx/daemon.py
+++ lbryumx/daemon.py
@@ -6,3 +6,7 @@ class LBCDaemon(Daemon):
     async def getclaimbyid(self, claim_id):
         '''Given a claim id, retrieves claim information.'''
         return await self._send_single('getclaimbyid', [claim_id])
+
+    async def getclaimsforname(self, name):
+        '''Given a name, retrieves all claims matching that name.'''
+        return await self._send_single('getclaimsforname', [name])
