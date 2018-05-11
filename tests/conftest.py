f6aa42d5a44aa07c7e9e869ceec377f6dc9751a6
Victor Shyba <victor1984@riseup.net>
2018-05-11 19:49:34 -0300

    add fixtures for functional tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,31 @@
 import json
-import os
 import pytest
+from electrumx.server.controller import Controller
+from xprocess import ProcessStarter
 from os import environ
-from lbryumx.coin import LBC
+from lbryumx.coin import LBC, LBCRegTest
 from electrumx.server.storage import Storage
 from electrumx.server.env import Env
+from urllib.request import urlopen
+import os
+import stat
+
+
+def download_lbrycrdd(path):
+    # TODO: get from lbrycrd repo when the changes are there
+    print("Downloading lbrycrdd into {}".format(path))
+    with open(path, "wb") as lbrycrdd_file:
+        req = urlopen("http://victor.lbry.tech:5000/lbrycrdd")
+        lbrycrdd_file.write(req.read())
+
+
+def ensure_lbrycrdd():
+    path = "/tmp/lbrycrdd"
+    if not os.path.isfile(path):
+        download_lbrycrdd(path)
+    st = os.stat(path)
+    os.chmod(path, st.st_mode | stat.S_IEXEC)
+    return path
 
 
 @pytest.fixture()
@@ -32,3 +53,28 @@ def block_infos():
         with open(block_file_path, 'r') as block_file:
             blocks[number] = json.loads(block_file.read())
     return blocks
+
+
+def setup_session(data_dir, rpc_port):
+    conf = {'DB_DIRECTORY': data_dir,
+            'DAEMON_URL': 'http://lbry:lbry@localhost:{}/'.format(rpc_port)}
+    os.environ.update(conf)
+    controller = Controller(Env(LBCRegTest))
+    session = LBC.SESSIONCLS(controller, 'TCP')
+    return session
+
+
+@pytest.fixture(scope="module")
+def regtest_session(xprocess, tmpdir_factory):
+    rpc_port = 1337
+    class RegtestLbryStarter(ProcessStarter):
+        pattern = ".*Done\ loading.*"
+        lbrycrdd_path = ensure_lbrycrdd()
+        data_path = "/tmp/regtest"
+        args = ['{}'.format(lbrycrdd_path), "-server", "-txindex", "-rpcuser=lbry", "-rpcpassword=lbry",
+                "-rpcport={}".format(rpc_port), "-datadir={}".format(data_path), "-printtoconsole", "-regtest"]
+
+    xprocess.ensure("lbrycrdd", RegtestLbryStarter)
+    xprocess.getinfo("lbrycrdd")
+    data_dir = tmpdir_factory.mktemp('db', numbered=True).strpath
+    return setup_session(data_dir, 1337)
