d2736c5aa72e568e2aa8f521af34b9de0fa11dec
Victor Shyba <victor1984@riseup.net>
2018-05-14 14:53:56 -0300

    add start script

---
+++ lbryumx_server.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+#import sys
+#import ujson
+#sys.modules['json'] = ujson
+
+import logging
+import traceback
+
+from electrumx.server.env import Env
+from electrumx.server.controller import Controller
+
+from lbryumx.coin import LBC
+
+def main():
+    '''Set up logging and run the server.'''
+    log_fmt = Env.default('LOG_FORMAT', '%(levelname)s:%(name)s:%(message)s')
+    logging.basicConfig(level=logging.INFO, format=log_fmt)
+    logging.info('LbryumX server starting')
+    try:
+        controller = Controller(Env(LBC))
+        controller.run()
+    except Exception:
+        traceback.print_exc()
+        logging.critical('LbryumX server terminated abnormally')
+    else:
+        logging.info('LbryumX server terminated normally')
+
+
+if __name__ == '__main__':
+    main()
